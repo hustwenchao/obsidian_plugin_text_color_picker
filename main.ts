ff171795f1a5b8decd9c880c37e1b5e865b8e26c
caiwenchao <caiwenchao@corp.netease.com>
2024-12-05 18:01:19 +0800

    init:create text color selecter plugin

--- main.ts
+++ main.ts
@@ -1,113 +1,70 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface TextColorSettings {
+	colors: string[];
+	colorEmojis: { [key: string]: string };
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_TEXT_COLOR_SETTINGS: TextColorSettings = {
+	colors: ['red', 'blue', 'green', 'purple', 'orange'],
+	colorEmojis: {
+		'red': '🔴',
+		'blue': '🔵',
+		'green': '🟢',
+		'purple': '🟣',
+		'orange': '🟠'
+	}
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	textColorSettings: TextColorSettings;
 
 	async onload() {
-		await this.loadSettings();
+		await this.loadTextColorSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// 为每个颜色添加命令
+		this.textColorSettings.colors.forEach(color => {
+			const colorEmoji = this.textColorSettings.colorEmojis[color] || '⬤';
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			this.addCommand({
+				id: `set-text-${color}`,
+				name: `Set Text To ${color} ${colorEmoji}`,
+				editorCallback: (editor: Editor) => {
+					this.setTextColor(editor, color);
 				}
-			}
+			});
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// 添加设置选项卡
+		this.addSettingTab(new TextColorSettingTab(this.app, this));
 	}
 
 	onunload() {
-
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async loadTextColorSettings() {
+		this.textColorSettings = Object.assign({}, DEFAULT_TEXT_COLOR_SETTINGS, await this.loadData());
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	async saveTextColorSettings() {
+		await this.saveData(this.textColorSettings);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	setTextColor(editor: Editor, color: string) {
+		const selectedText = editor.getSelection();
+		if (selectedText) {
+			const coloredText = `<span style="color: ${color}">${selectedText}</span>`;
+			editor.replaceSelection(coloredText);
+		}
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	reloadPlugin() {
+		this.unload();
+		this.load();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class TextColorSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -117,18 +74,74 @@ class SampleSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
+		containerEl.createEl('h2', {text: 'Text Color Settings'});
+
+		// 颜色列表容器
+		const colorListContainer = containerEl.createDiv('color-list-container');
+		
+		// 添加新颜色的设置
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Add New Color')
+			.setDesc('Add a new color to the list')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+				.setPlaceholder('Color name (e.g., red)')
+				.setValue('')
+				.onChange(() => {}))
+			.addText(text => text
+				.setPlaceholder('Emoji (e.g., 🔴)')
+				.setValue('')
+				.onChange(() => {}))
+			.addButton(button => button
+				.setButtonText('Add')
+				.onClick(async () => {
+					const colorInput = containerEl.querySelector('.add-color-input') as HTMLInputElement;
+					const emojiInput = containerEl.querySelector('.add-color-emoji-input') as HTMLInputElement;
+					const color = colorInput?.value.trim();
+					const emoji = emojiInput?.value.trim();
+
+					if (color && emoji) {
+						this.plugin.textColorSettings.colors.push(color);
+						this.plugin.textColorSettings.colorEmojis[color] = emoji;
+						await this.plugin.saveTextColorSettings();
+						this.plugin.reloadPlugin();
+						this.display(); // 刷新设置界面
+						colorInput.value = '';
+						emojiInput.value = '';
+					}
 				}));
+
+		// 显示现有颜色列表
+		this.plugin.textColorSettings.colors.forEach(color => {
+			const emoji = this.plugin.textColorSettings.colorEmojis[color] || '⬤';
+			new Setting(colorListContainer)
+				.setName(color)
+				.setDesc(`Current emoji: ${emoji}`)
+				.addText(text => text
+					.setPlaceholder('New emoji')
+					.setValue('')
+					.onChange(async (value) => {
+						if (value.trim()) {
+							this.plugin.textColorSettings.colorEmojis[color] = value.trim();
+							await this.plugin.saveTextColorSettings();
+							this.plugin.reloadPlugin();
+							this.display(); // 刷新设置界面
+						}
+					}))
+				.addButton(button => button
+					.setButtonText('Delete')
+					.setClass('color-delete-button')
+					.onClick(async () => {
+						const index = this.plugin.textColorSettings.colors.indexOf(color);
+						if (index > -1) {
+							this.plugin.textColorSettings.colors.splice(index, 1);
+							delete this.plugin.textColorSettings.colorEmojis[color];
+							await this.plugin.saveTextColorSettings();
+							this.plugin.reloadPlugin();
+							this.display(); // 刷新设置界面
+						}
+					}));
+		});
 	}
 }
